6e70cb2206ab8d477ef4d2711407314e8656c2f8
Marckvdv <Marckvandervegt@gmail.com>
2016-04-17 18:42:58 +0200

    Shaders are now included in the binary

--- src/mandelbrot_shader.rs
+++ src/mandelbrot_shader.rs
@@ -12,15 +12,8 @@ pub struct Shaders {
 
 impl Shaders {
     pub fn from_sources(display: &GlutinFacade, vert_path: &str, frag_path: &str) -> Shaders {
-        let (vert_path, frag_path) = (Path::new(vert_path), Path::new(frag_path));
-        let (mut vert_file, mut frag_file) = (File::open(vert_path).unwrap(), File::open(frag_path).unwrap());
-        let (mut vert_src, mut frag_src) = (String::new(), String::new());
-
-        vert_file.read_to_string(&mut vert_src);
-        frag_file.read_to_string(&mut frag_src);
-
         Shaders {
-            program: Program::from_source(display, &vert_src, &frag_src, None).unwrap()
+            program: Program::from_source(display, include_str!("vert.glsl"), include_str!("frag.glsl"), None).unwrap()
         }
     }
 }
